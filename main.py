8ad71d43eea8bf0edcc2ed8dc74b427951e2fe38
Rajkumar Bhuva <rrb.bhuva@gmail.com>
2024-04-27 18:06:12 -0400

    update and delete methond for customer added

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def create_cutomer(customer:Customer):
     conn.commit()
     conn.close()
     return customer
-    
 
 @app.get("/customers/{cust_id}")
 def read_item(cust_id: int, q=None):
@@ -37,13 +36,34 @@ def read_item(cust_id: int, q=None):
     customer = curr.fetchone()
     conn.close()
     if customer != None:
-        return{
-            "id" : customer[0],
-            "name": customer[1],
-            "phone": customer[2]
-        }
-    
+        return Customer(cust_id = customer[0], name= customer[1],  phone=customer[2])
     else:
         raise HTTPException(status_code=404, detail="Customer not found")
-        
-        
+
+@app.put("/customers/{cust_id}")
+def update_customer(cust_id:int , customer: Customer):
+    if customer.cust_id != None and customer.cust_id != cust_id:
+        raise  HTTPException(status_code=400, detail="Customer ID doesn't match URL")
+    
+    customer.cust_id = cust_id
+    conn = sqlite3.connect("db.sqlite")
+    curr = conn.cursor()
+    curr.execute("UPDATE customers SET name=?,phone=? Where id=?;", (customer.name, customer.phone, cust_id))
+    total_changes = conn.total_changes
+    conn.commit()
+    conn.close()
+    if total_changes == 0:
+        raise   HTTPException(status_code=404,detail="Customer data not found")
+    return customer
+
+@app.delete("/customers/{cust_id}")
+def delete_customer(cust_id : int):
+    conn = sqlite3.connect("db.sqlite")
+    curr = conn.cursor()
+    curr.execute("DELETE from customers WHERE id=?;", (cust_id,))
+    total_changes = conn.total_changes
+    conn.commit()
+    conn.close()
+    if total_changes != 1:
+        raise HTTPException(status_code=400, detail=f"{total_changes} not found")
+    return total_changes
